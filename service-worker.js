8abe5663a6d67f5f39ddea13addb067db7543849
Rishabh Kumar <rishabh.kumar@liferay.com>
2022-09-18 10:39:59 +0530

    Events for servie worker

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,16 @@
-/*Service Worker*/
+/*Service Worker Events*/
+self.addEventListener('install',(evt) =>{
+    console.log("Service worker install");
+})
+
+self.addEventListener('active',(evt) =>{
+    console.log("Service worker active");
+})
+
+self.addEventListener('fetch',(evt) =>{
+    console.log("doing a fetch call or http request",evt.request);
+})
+
+self.addEventListener('message',(evt) =>{
+    console.log("message from web page");
+})
